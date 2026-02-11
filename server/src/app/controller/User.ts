aea293e2c42daca2bb0ac7d413e35d8d931a32f6
hereisphil <thereisphil@gmail.com>
2026-02-10 18:52:26 -0600

    SERVER: Major update/refactoring. Updated created controller/Google.ts & User.s and updated routes/index.ts; Now the JWT being created only contain the Google Id to prevent sensitive information in the clean & new route /api/v1/auth/token which will receive the token from the frontend, decode it, and determine if the user within the payload exists, and determine if the token is expired.
    MUST WORK ON CLIENT

---
+++ server/src/app/controller/User.ts
@@ -0,0 +1,39 @@
+import type { RequestHandler } from "express";
+import UserModel from "../model/User.js";
+import { decodeJWTPayload } from "../utils/decodeJwtPayload.js";
+
+/* -------------------------------------------------------------------------- */
+/*                       GET Authenticated User Details                       */
+/* -------------------------------------------------------------------------- */
+export const getAuthenticatedUser: RequestHandler = async (req, res) => {
+    try {
+        const { authorization} = await req.headers;
+        if(!authorization) return res.status(400).json({
+            message: "User not logged in",
+            success: false,
+        })
+        const payload = await decodeJWTPayload(authorization)
+        console.log(payload);
+        const expiration = payload.exp;
+        const now = Math.floor(Date.now() / 1000);
+        if (expiration && expiration < now) {
+            return res.status(401).json({
+                message: "Token expired",
+                success: false,
+            });
+        }
+
+        const existingUser = await UserModel.findOne({ googleId: payload.id }).exec();
+        if(!existingUser) return res.status(400).json({
+            message: "User not found",
+            success: false
+        });
+        return res.status(200).json(existingUser);
+    } catch (err) {
+        return res.status(500).json({
+            message: "Internal error",
+            success: false
+        })
+    }
+};
+
