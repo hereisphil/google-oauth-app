45b263264ff23b332386284f5b13f72e4e1fe43b
hereisphil <thereisphil@gmail.com>
2026-02-11 11:32:36 -0600

    CLIENT: followed ui.shadcn.com/docs/installation/vite docs to install, configure, and initialize tailwindcss+shadcnui

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import { TokenProvider } from "./context/TokenContext.tsx";
+import "./styles/index.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
