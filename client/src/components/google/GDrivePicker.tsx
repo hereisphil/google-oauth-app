b0db4ed3d21437342daa1939fe1cf931008a91a7
hereisphil <thereisphil@gmail.com>
2026-02-12 14:14:23 -0600

    CLIENT: installed the official @googleworkspace/drive-picker-react and created GDrivePicker.tsx based off of Google's official example https://github.com/googleworkspace/drive-picker-element/blob/main/examples/react/src/App.tsx - it's a start but it's FAR from being what I want.

---
+++ client/src/components/google/GDrivePicker.tsx
@@ -0,0 +1,36 @@
+import {
+    DrivePicker,
+    DrivePickerDocsView,
+} from "@googleworkspace/drive-picker-react";
+import React from "react";
+
+const GDrivePicker = () => {
+    const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+    const APP_ID = import.meta.env.VITE_APP_ID;
+
+    const [events, setEvents] = React.useState<unknown[]>([]);
+
+    return (
+        <>
+            <DrivePicker
+                client-id={CLIENT_ID}
+                app-id={APP_ID}
+                onOauthResponse={(e) => setEvents([...events, e.detail])}
+                onPicked={(e) => setEvents([...events, e.detail])}
+                onCanceled={(e) => setEvents([...events, e.detail])}
+            >
+                <DrivePickerDocsView
+                    owned-by-me="true"
+                    mime-types="application/vnd.google-apps.spreadsheet"
+                />
+            </DrivePicker>
+            {events.map((event, index) => (
+                <pre className="event" key={index}>
+                    {JSON.stringify(event, null, 2)}
+                </pre>
+            ))}
+        </>
+    );
+};
+
+export default GDrivePicker;
--- client/src/pages/UserInfoPage.tsx
+++ client/src/pages/UserInfoPage.tsx
@@ -1,9 +1,12 @@
+import GDrivePicker from "@/components/google/GDrivePicker";
 import { Button } from "@/components/ui/button";
 import logout from "@/utils/logout";
+import { useState } from "react";
 import { useToken } from "../utils/useToken";
 
 export function UserInfoPage() {
     const { user } = useToken();
+    const [showPicker, setShowPicker] = useState(false);
 
     return (
         <main style={{ padding: "2rem" }}>
@@ -12,6 +15,17 @@ export function UserInfoPage() {
             <pre style={{ marginTop: "1rem" }}>
                 {JSON.stringify(user, null, 2)}
             </pre>
+
+            {showPicker ? (
+                <GDrivePicker />
+            ) : (
+                <Button
+                    style={{ marginTop: "1rem" }}
+                    onClick={() => setShowPicker(!showPicker)}
+                >
+                    Show Google Drive Picker
+                </Button>
+            )}
 
             <Button style={{ marginTop: "1rem" }} onClick={logout}>
                 Log out
