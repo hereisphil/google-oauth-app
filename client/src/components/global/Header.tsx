de11b0ea6afede0ec909204467eeb08e905348f4
hereisphil <thereisphil@gmail.com>
2026-02-17 20:15:29 -0600

    CLIENT: bigger homepage CTA button, changed Button to Link in Header & updated styles & added icons

--- client/src/components/global/Header.tsx
+++ client/src/components/global/Header.tsx
@@ -1,28 +1,37 @@
 import { Button } from "@/components/ui/button";
 import logout from "@/utils/logout";
 import { useToken } from "@/utils/useToken";
-import { useNavigate } from "react-router";
+import { Hash, LogOut, User } from "lucide-react";
+import { Link, useNavigate } from "react-router";
 
 const Header = () => {
     const { user } = useToken();
     const navigate = useNavigate();
 
     return (
-        <header className="flex justify-between items-center p-4 mb-4 border-b bg-gray-100">
-            <Button variant="link" onClick={() => navigate("/")}>
+        <header className="flex justify-between items-center p-4 mb-4 border-b bg-accent">
+            <Link
+                to="/"
+                className="font-bold text-lg md:text-xl hover:underline"
+            >
+                <Hash className="inline-block" />
                 Google OAuth App
-            </Button>
+            </Link>
             {user ? (
-                <div className="flex gap-2 items-center">
-                    <span className="font-bold text-lg">
+                <div className="flex flex-col md:flex-row gap-2 items-center">
+                    <span className="font-bold text-sm md:text-lg">
                         Hello, {user.name}!
                     </span>
-                    <Button size="sm" onClick={() => navigate("user-info")}>
-                        Profile
-                    </Button>
-                    <Button variant="destructive" size="sm" onClick={logout}>
-                        Log out
-                    </Button>
+                    <div className="flex gap-2 justify-center items-center">
+                        <Button size="sm" onClick={() => navigate("user-info")}>
+                            <User className="mr-0.5" />
+                            Profile
+                        </Button>
+                        <Button variant="ghost" size="xs" onClick={logout}>
+                            <LogOut className="mr-0.5" />
+                            Log out
+                        </Button>
+                    </div>
                 </div>
             ) : (
                 <Button onClick={() => navigate("/login")}>Log in</Button>
