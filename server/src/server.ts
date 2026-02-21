cb7bb3dee0d491b181db828404f1740f31d072c4
hereisphil <thereisphil@gmail.com>
2026-02-20 20:24:42 -0600

    SERVER Attempt to setup for Vercel

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,24 @@
-import "dotenv/config";
-import app from "./app/app.js";
+import cors from "cors";
+import express, { type Request, type Response } from "express";
+import morgan from "morgan";
 import connectDB from "./app/db/config.js";
+import routeHandler from "./app/routes/index.js";
 
 await connectDB();
 
-const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cors());
 
-app.listen(PORT, () => {
-    console.log(`NodeJS/Express Server running on http://localhost:${PORT}`);
+app.get("/", (_req: Request, res: Response) => {
+    res.status(200).json({
+        message: "Server is running.",
+        success: true,
+    });
 });
+
+// API Routes
+app.use("/api/v1", routeHandler);
+
+export default app;
